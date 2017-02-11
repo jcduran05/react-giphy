c514c7e429af12f426dafcc214003b686f4d4451
jcduran05 <jcduran05@gmail.com>
2017-02-10 22:57:38 -0800

    Basic search to giphy api and display results in a list.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import axios from 'axios';
+import GifList from './components/GifList';
+import SearchBar from './components/SearchBar';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: []
+    }
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  handleTermChange(term) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=8`;
+
+    axios.get(url)
+    .then(res => {
+      this.setState({ gifs: res.data.data })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onTermChange={term => this.handleTermChange(term)} />
+        <GifList gifs={this.state.gifs} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
