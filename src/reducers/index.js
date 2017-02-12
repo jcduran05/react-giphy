104f258c500bb59ecf147a44c3ad30a934efc581
jcduran05 <jcduran05@gmail.com>
2017-02-12 13:15:11 -0800

    Signup and login forms created through redux-form.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import GifsReducer from './gifs';
 import ModalReducer from './modal';
+import  { reduxer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   gifs: GifsReducer,
   modal: ModalReducer
 });
