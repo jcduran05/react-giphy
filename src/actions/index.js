0339c287fe95aad4870403b8054508e7159d9f3d
jcduran05 <jcduran05@gmail.com>
2017-02-12 14:40:33 -0800

    Hooked up sign-up and login to work with firebase.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,37 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import Firebase from 'firebase';
 
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const REQUEST_GIFS = 'REQUEST_GIFS';
 export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const AUTH_USER = 'AUTH_USER';
 
 const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = `&api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=8`
 
-export function requestGifs(term = null) {
-  const data = axios.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
+var config = {
+  apiKey: "AIzaSyDIeysaU3-HWaeglltxW52cUnICDZIsJDc",
+  authDomain: "react-gif-search-84fa5.firebaseapp.com",
+  databaseURL: "https://react-gif-search-84fa5.firebaseio.com",
+  storageBucket: "react-gif-search-84fa5.appspot.com",
+  messagingSenderId: "993423880833"
+};
 
-  return {
-    type: REQUEST_GIFS,
-    payload: data
+Firebase.initializeApp(config);
+
+export function requestGifs(term = null) {
+  return function(dispatch) {
+    axios.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`)
+    .then(response => {
+      dispatch({
+        type: REQUEST_GIFS,
+        payload: response
+      });
+    });
   }
 }
 
@@ -32,20 +48,61 @@ export function closeModal() {
   }
 }
 
-
-export function signInUser() {
-  browserHistory.push('/favorites');
-
-  return {
-    type: SIGN_IN_USER
+export function signUpUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+        browserHistory.push('/favorites');
+      })
+      .catch(error => {
+        dispatch(authError(error));
+      });
   }
 }
 
+export function signInUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+        browserHistory.push('/favorites');
+      })
+      .catch(error => {
+        dispatch(authError(error));
+      });
+  }
+}
 
 export function signOutUser() {
   browserHistory.push('/');
 
   return {
     type: SIGN_OUT_USER
+  }
+}
+
+export function verifyAuth() {
+  return function (dispatch) {
+    Firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        dispatch(authUser());
+      } else {
+        dispatch(signOutUser());
+      }
+    });
+  }
+}
+
+export function authUser() {
+  return {
+    type: AUTH_USER
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
